2aa8b16cb7da20a58fec52240bcfbbad937087e7
uever.martins@gmail.com <72147393+u-ever@users.noreply.github.com>
2021-06-10 19:01:40 -0300

    Primeiros exercícios

---
+++ exercises/ex002.py
@@ -0,0 +1,4 @@
+dia = input ('Que dia você nasceu?')
+mes = input ('Que mês você nasceu?')
+ano = input ('E em que ano você nasceu?')
+print ('Ah, que legal! Então você nasceu em', dia, 'de', mes, 'de', ano,'.')
