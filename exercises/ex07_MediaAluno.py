0164db157d00acfb61db62a8573a12a3a3ac2cc7
uever.martins@gmail.com <72147393+u-ever@users.noreply.github.com>
2021-06-11 23:01:33 -0300

    ex07_Media do  Aluno

---
+++ exercises/ex07_MediaAluno.py
@@ -0,0 +1,4 @@
+n1 =float(input('Digite a primeira nota '))
+n2 =float(input('Digite a segunda nota '))
+m = (n1 + n2) / 2
+print('A média entre as notas {} e {} do aluno é {:.1F}.'.format(n1, n2, m))
