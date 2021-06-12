9e88d54e1c2808c4a71021a4f30e1a0df7e08e6e
uever.martins@gmail.com <72147393+u-ever@users.noreply.github.com>
2021-06-11 23:00:59 -0300

    ex06_DOBRO_TRIPLO_RAIZ_Q

---
+++ exercises/ex06_DOBRO_TRIPLO_RAIZ_Q.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número '))
+d =n * 2
+t =n * 3
+r =n ** (1/2)
+print('O dobro de {} número é {}.\nO triplo é {}.\nE a raiz quadrada é {:.2F}.'.format(n, d, t, r))
---
+++ exercises/ex06b_DOBRO_TRIPLO_RAIZ_Q.py
@@ -0,0 +1,2 @@
+n = int(input('Digite um número '))
+print('O dobro de {} número é {}.\nO triplo é {}.\nE a raiz quadrada é {:.2F}.'.format(n, n * 2, (n * 3), pow (n,(1/2))))
