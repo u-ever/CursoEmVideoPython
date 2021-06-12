71c0d4da94b49e23ab0fecba3f95116cb44d0f8c
uever.martins@gmail.com <72147393+u-ever@users.noreply.github.com>
2021-06-11 22:59:56 -0300

    ex05 Sucessor Antecessor

---
+++ exercises/ex05_Sucessor_Antecessor.py
@@ -0,0 +1,4 @@
+n1 =(int(input("Digite um número ")))
+sus =n1 + 1
+ant =n1 - 1
+print('Analisando o número {} o seu sucessor é o {} e o antecessor é o {}.'.format(n1, sus, ant))
---
+++ exercises/ex05b_Sucessor_Antecessor.py
@@ -0,0 +1,4 @@
+n= (int(input('Digite um número ')))
+print('Analisando o número {} o seu sucessor é o {} e o antecessor é o {}.'.format(n, n + 1,n - 1))
+
+#Esta solução não possui variaveis, neste caso é impossivel utilizar estes numeros se eles forem necessários posteriormente. Esta formatação economiza memória.
