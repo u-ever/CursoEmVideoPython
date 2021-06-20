4615d8542831e845cd4d2902f59aeca1b8e2bba0
u-ever <uever.martins@gmail.com>
2021-06-19 21:57:24 -0300

    tratamento de caracteres

---
+++ exercises/ex024__Verificando_as_primeiras_letras_de_um_texto.py
@@ -0,0 +1,3 @@
+c =str(input('Em que cidade você nasceu? ')).strip() #strip necessário na função sem o IN
+#print('Santo' in c.capitalize())
+print(c[:5].capitalize() == 'Santo')
---
+++ exercises/ex025_Procurando_uma_string_dentro_de_outra.py
@@ -0,0 +1,2 @@
+n =str(input('Qual é seu nome completo? ')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in n.lower()))
